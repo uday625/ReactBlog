e1356e99487f5558511fcd0d0d368ba60d01cab3
UDAY BAG <uday625@Nemesis-2.local>
2018-12-08 10:28:26 -0500

    added base layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import Blog from './containers/Blog/Blog';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <p> Welcome to React </p>
+          <Blog/>
 
       </div>
     );
